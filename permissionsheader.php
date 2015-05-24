3e56693643bca4ba2fa3ce0864de6a7d3c25b3a3
James W <timeimp@icloud.com>
2015-05-24 15:33:18 +1000

    PHP - use global variables for the header.

--- permissionsheader.php
+++ permissionsheader.php
@@ -13,12 +13,13 @@ require('php/Direct/config/permission.inc.php');
 require('php/Direct/auth/permissions.engine.php');
 
 $PERMISSIONS_ENGINE = new PermissionsEngine();
-
 /* 
   1) Find the user's place in this project
   2) If they do not have one, find the project default's
   3) If they do have one, get the Role's permission
 */
+global $userRole;
+global $rolePermissions;
 
 $userRole = new DBPermAssignmentMappingsModel(1, 1);
 $rolePermissions = new DBRoleInformationModel($userRole->getAssignmentModel()->getRoleMappingID(), 1);
