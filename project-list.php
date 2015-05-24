47917f6f7405ae4512115b51e62e89a1181e7218
James W <timeimp@icloud.com>
2015-05-24 15:33:53 +1000

    PHP - show permission denied message on Project List page.

--- project-list.php
+++ project-list.php
@@ -1,6 +1,31 @@
 <?php 
 require('permissionsheader.php');
-include_once('templates/header.php'); 
+require('templates/header.php'); 
+
+if(!$PERMISSIONS_ENGINE->canCompleteRequestForOperation($rolePermissions->getRoleInfo()->getPermissionsValue(), P_FULL_CONTROL))
+{
+  ?>
+<div class="row content-container">
+	<div class="col-xs-12"><a href="task-list.php">Tasks</a></div>
+</div>
+<?php 
+  
+if($PERMISSIONS_ENGINE->canCompleteRequestForOperation($rolePermissions->getRoleInfo()->getPermissionsValue(), (P_FULL_CONTROL | P_CREATE)))
+{ 
+  echo '<div class="row content-container visible-xs visible-sm">';
+  require_once('templates/create-buttons.php');
+  echo '</div>';
+}
+
+?>
+<div id="project-list" class="row content-container">
+	<h4>Permission Denied</h4>
+	<p>You do not have Full Control access. You may not view this page.</p>
+</div>
+<?php
+  include('templates/footer.php');
+}
+
 
 if($PERMISSIONS_ENGINE->canCompleteRequestForOperation($rolePermissions->getRoleInfo()->getPermissionsValue(), P_FULL_CONTROL))
   include_once('templates/create-project.php'); 
