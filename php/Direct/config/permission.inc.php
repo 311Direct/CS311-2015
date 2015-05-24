51fed5f3c34e2efa478772c41f5fc833f255ba7a
James W <timeimp@icloud.com>
2015-05-24 13:34:07 +1000

    PHP - updated permissions constants

--- php/Direct/config/permission.inc.php
+++ php/Direct/config/permission.inc.php
@@ -1,21 +1,28 @@
 <?php
 
-const P_FULL_CONTROL    = 0b100000000;
-const P_READ            = 0b010000000;
-const P_WRITE           = 0b001000000;
-const P_CREATE          = 0b000100000;
-const P_DELETE          = 0b000010000;
-const P_MODIFY_ATTR     = 0b000001000;
-const P_CHANGE_ACCESS   = 0b000000100;
-const P_LIST_CONTENTS   = 0b000000010;
-const P_INHERITS_PARENT = 0b000000001;
-
+const P_FULL_CONTROL    = 0b100000000000;
+const P_READ            = 0b010000000000;
+const P_WRITE           = 0b001000000000;
+const P_CREATE          = 0b000100000000;
+const P_DELETE          = 0b000010000000;
+const P_MODIFY_ATTR     = 0b000001000000;
+const P_CHANGE_ACCESS   = 0b000000100000;
+const P_LIST_CONTENTS   = 0b000000010000;
+const P_INHERITS_PARENT = 0b000000000001;
+const P_PERMISSION_UNDEF= 0b000000000000;
+const P_ACTION_ADD      = 0b000000001000;
+const P_ACTION_UPDATE   = 0b000000000100;
+const P_ACTION_DELETE   = 0b000000000010;
 
 define("P_MAX_ENTRY", P_FULL_CONTROL | P_READ | P_WRITE | P_CREATE | P_DELETE | P_MODIFY_ATTR | P_CHANGE_ACCESS | P_LIST_CONTENTS | P_INHERITS_PARENT);
 define("P_ROOT_LEVEL", 1000 + ( P_FULL_CONTROL | P_READ | P_WRITE | P_CREATE | P_DELETE | P_MODIFY_ATTR | P_CHANGE_ACCESS | P_LIST_CONTENTS));
 
-// Unpacking will involve first int: project ID, second char: User or Role, third int: primary DB id, fourth value: int of above consts.
+define("P_ACTION_MIN", (P_ACTION_DELETE & P_ACTION_UPDATE & P_ACTION_ADD));
+define("P_ACTION_MAX", (P_ACTION_ADD | P_ACTION_UPDATE | P_ACTION_DELETE));
 
-const P_DB_FORMAT_V1 = "IA1IIx";
-const P_DB_UFORMT_V1 = "IP/AT/IW/IV/x"
+function noActionSet($aPerms)
+{
+  return true === ((($aPerms & P_ACTION_ADD) == P_ACTION_ADD) || (($aPerms & P_ACTION_UPDATE) == P_ACTION_UPDATE) || (($aPerms & P_ACTION_DELETE) == P_ACTION_DELETE));
+}
+
 ?>
