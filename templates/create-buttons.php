7e317e5b527e7b29ef78b48a20e7ad7baae87512
James W <timeimp@icloud.com>
2015-05-24 14:51:53 +1000

    PHP - buttons now have finer grained permissions.

--- templates/create-buttons.php
+++ templates/create-buttons.php
@@ -2,10 +2,10 @@
 if($PERMISSIONS_ENGINE->canCompleteRequestForOperation($rolePermissions->getRoleInfo()->getPermissionsValue(), P_FULL_CONTROL))
   echo '<button class="col-xs-6 btn-create-project" data-item-type="project"><span class="glyphicon glyphicon-plus"></span> Project</button>';
   
-if($PERMISSIONS_ENGINE->canCompleteRequestForOperation($rolePermissions->getRoleInfo()->getPermissionsValue(), P_CREATE))
+if($PERMISSIONS_ENGINE->canCompleteRequestForOperation($rolePermissions->getRoleInfo()->getPermissionsValue(), (P_FULL_CONTROL | P_CREATE)))
   echo '<button class="col-xs-6 btn-create-milestone" data-item-type="milestone"><span class="glyphicon glyphicon-plus"></span> Milestone</button>';
 
-if($PERMISSIONS_ENGINE->canCompleteRequestForOperation($rolePermissions->getRoleInfo()->getPermissionsValue(), P_CREATE))
+if($PERMISSIONS_ENGINE->canCompleteRequestForOperation($rolePermissions->getRoleInfo()->getPermissionsValue(), (P_FULL_CONTROL | P_CREATE)))
   echo '<button class="col-xs-6 btn-create-task" data-item-type="task"><span class="glyphicon glyphicon-plus"></span> Task</button>';
 
 if($PERMISSIONS_ENGINE->canCompleteRequestForOperation($rolePermissions->getRoleInfo()->getPermissionsValue(), P_FULL_CONTROL))
